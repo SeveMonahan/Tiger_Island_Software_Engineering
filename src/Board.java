364d469d6936e515cce6f2a9d3a5eae14df835d0
Seve Monahan <monahan.seve@gmail.com>
2017-03-06 13:57:19 -0500

    Test the algorithm which finds all the of a Hex's neighbors.

--- src/Board.java
+++ src/Board.java
@@ -17,4 +17,26 @@ class Board {
     void setHexagonUsingPrivateIndexingForTest(int x, int y, Hexagon hex){
         hexagonArray[x][y] = hex;
     }
+
+    Hexagon[] getNeighbors(int x, int y){
+        Hexagon[] neighbors = new Hexagon[6];
+
+        neighbors[0] = hexagonArray[x+1][y];
+        neighbors[1] = hexagonArray[x-1][y];
+        neighbors[2] = hexagonArray[x][y+1];
+        neighbors[3] = hexagonArray[x][y-1];
+
+        int offset;
+
+        if(x % 2 == 1) {
+            offset = 1;
+        }else{
+            offset = -1;
+        }
+
+        neighbors[4] = hexagonArray[x+offset][y+1];
+        neighbors[5] = hexagonArray[x+offset][y-1];
+
+        return neighbors;
+    }
 }
--- test/BoardTest.java
+++ test/BoardTest.java
@@ -31,4 +31,52 @@ public class BoardTest {
         assertEquals(1, ReturnedHexagon.getlevel());
 
     }
+
+    @Test
+    public void getNeighborsOddX() throws Exception{
+        Board TestBoard = new Board();
+
+        Hexagon TestHexagon = new Hexagon();
+
+        TestHexagon.incrementlevel();
+
+        TestBoard.setHexagonUsingPrivateIndexingForTest(102, 101, TestHexagon);
+        TestBoard.setHexagonUsingPrivateIndexingForTest(100, 101, TestHexagon);
+
+        TestBoard.setHexagonUsingPrivateIndexingForTest(101, 100, TestHexagon);
+        TestBoard.setHexagonUsingPrivateIndexingForTest(101, 102, TestHexagon);
+
+        TestBoard.setHexagonUsingPrivateIndexingForTest(102, 100, TestHexagon);
+        TestBoard.setHexagonUsingPrivateIndexingForTest(102, 102, TestHexagon);
+
+        Hexagon[] neighbors = TestBoard.getNeighbors(101,101);
+
+        for(int i=0; i<6; i++){
+            assertEquals(1, neighbors[i].getlevel());
+        }
+    }
+
+    @Test
+    public void getNeighborsEvenX() throws Exception{
+        Board TestBoard = new Board();
+
+        Hexagon TestHexagon = new Hexagon();
+
+        TestHexagon.incrementlevel();
+
+        TestBoard.setHexagonUsingPrivateIndexingForTest(53, 52, TestHexagon);
+        TestBoard.setHexagonUsingPrivateIndexingForTest(51, 52, TestHexagon);
+
+        TestBoard.setHexagonUsingPrivateIndexingForTest(52, 51, TestHexagon);
+        TestBoard.setHexagonUsingPrivateIndexingForTest(52, 53, TestHexagon);
+
+        TestBoard.setHexagonUsingPrivateIndexingForTest(51, 51, TestHexagon);
+        TestBoard.setHexagonUsingPrivateIndexingForTest(51, 53, TestHexagon);
+
+        Hexagon[] neighbors = TestBoard.getNeighbors(52,52);
+
+        for(int i=0; i<6; i++){
+            assertEquals(1, neighbors[i].getlevel());
+        }
+    }
 }
