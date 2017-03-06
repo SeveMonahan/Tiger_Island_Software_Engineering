ac1fc8d5935c5cdf0380bf9ca7f87762c3c26d0a
Seve Monahan <monahan.seve@gmail.com>
2017-03-06 17:20:21 -0500

    Add getNextClockwise method to direction enum

--- src/DirectionsHex.java
+++ src/DirectionsHex.java
@@ -4,5 +4,25 @@ public enum DirectionsHex {
     LEFT,
     RIGHT,
     LOWERLEFT,
-    LOWERRIGHT
+    LOWERRIGHT;
+
+    DirectionsHex getNextClockwise(){
+        switch(this) {
+            case UPPERLEFT:
+                return UPPERRIGHT;
+            case UPPERRIGHT:
+                return LEFT;
+            case LEFT:
+                return LOWERLEFT;
+            case LOWERLEFT:
+                return LOWERRIGHT;
+            case LOWERRIGHT:
+                return RIGHT;
+            case RIGHT:
+                return UPPERLEFT;
+        }
+
+        // Error result
+        return DirectionsHex.LEFT;
+    }
 }
