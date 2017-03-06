7e49beabc20e53ce076871df01b863a3e531478b
Seve Monahan <monahan.seve@gmail.com>
2017-03-06 11:47:35 -0500

    Add Board class boilerplate

---
+++ test/BoardTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class BoardTest {
+    @Test
+    public void createBoard() throws Exception{
+        Board TestBoard = new Board();
+    }
+
+}
