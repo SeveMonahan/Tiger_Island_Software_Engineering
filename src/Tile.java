c29e22b3e414d52c8bf1607155f76d8559f7451d
Seve Monahan <monahan.seve@gmail.com>
2017-03-06 15:14:40 -0500

    Add skeletal tile class

---
+++ src/Tile.java
@@ -0,0 +1,5 @@
+public class Tile {
+    Tile(Terrain terrain_1, Terrain terrain_2){
+
+    }
+}
---
+++ test/TileTest.java
@@ -0,0 +1,8 @@
+import org.junit.Test;
+
+public class TileTest {
+    @Test
+    public void init() {
+        Tile TestTile = new Tile(Terrain.JUNGLE, Terrain.ROCK);
+    }
+}
