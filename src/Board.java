ff2fa1b703e6e157d5d2a4ff56a0cf7cd5e7c198
Seve Monahan <monahan.seve@gmail.com>
2017-03-07 01:06:41 -0500

    Reorganize order of methods in Board class

--- src/Board.java
+++ src/Board.java
@@ -6,13 +6,12 @@ class Board {
     // Higher y value is going "up" while higher x value is going "right."
     private Hexagon[][] hexagonArray;
 
-    private void initalizeHexagonArray() {
-        hexagonArray = new Hexagon[200][200];
-        for(int i = 0; i < 200; i++) {
-            for(int j = 0; j < 200; j++) {
-                hexagonArray[i][j] = new Hexagon();
-            }
-        }
+    Hexagon getHexagon(int x, int y){
+        return hexagonArray[x][y];
+    }
+
+    void setHexagon(int x, int y, Hexagon hex){
+        hexagonArray[x][y] = hex;
     }
 
     Board(){
@@ -23,6 +22,16 @@ class Board {
         initalizeHexagonArray();
         placeTileNoRestrictions(first_tile, DirectionsHex.LEFT, 100, 100);
     }
+
+    private void initalizeHexagonArray() {
+        hexagonArray = new Hexagon[200][200];
+        for(int i = 0; i < 200; i++) {
+            for(int j = 0; j < 200; j++) {
+                hexagonArray[i][j] = new Hexagon();
+            }
+        }
+    }
+
 
     public void placeTileNoRestrictions(Tile tile, DirectionsHex direction, int x, int y){
         hexagonArray[x][y].changeTerrainTypeThoughExplosion(Terrain.VOLCANO);
@@ -37,54 +46,6 @@ class Board {
         overwritten_3.changeTerrainTypeThoughExplosion(
                 tile.getTerrainsClockwiseFromVolcano()[2]);
 
-    }
-
-    private Hexagon getHexagonNeighbor(int x, int y, DirectionsHex direction){
-        switch(direction){
-            case LEFT:
-                return hexagonArray[x-1][y];
-            case RIGHT:
-                return hexagonArray[x+1][y];
-            case UPPERLEFT:
-                return hexagonArray[x-1 + offset(y)][y+1];
-            case UPPERRIGHT:
-                return hexagonArray[x + offset(y)][y+1];
-            case LOWERLEFT:
-                return hexagonArray[x-1 + offset(y)][y-1];
-            case LOWERRIGHT:
-                return hexagonArray[x + offset(y)][y-1];
-        }
-        // Effectively an Error
-        return hexagonArray[0][0];
-    }
-
-    Hexagon getHexagon(int x, int y){
-        return hexagonArray[x][y];
-    }
-
-    void setHexagon(int x, int y, Hexagon hex){
-        hexagonArray[x][y] = hex;
-    }
-
-    int offset(int x){
-        if(x % 2 == 1) {
-            return 1;
-        }else{
-            return 0;
-        }
-    }
-
-    Hexagon[] getNeighbors(int x, int y){
-        Hexagon[] neighbors = new Hexagon[6];
-
-        int i = 0;
-
-        for(DirectionsHex direction : DirectionsHex.values()) {
-            neighbors[i] = getHexagonNeighbor(x, y, direction);
-            i++;
-        }
-
-        return neighbors;
     }
 
     public boolean placeTile(Tile tile, DirectionsHex direction, int x, int y) {
@@ -107,4 +68,45 @@ class Board {
         }
         return found_attach_point;
     }
+
+    Hexagon[] getNeighbors(int x, int y){
+        Hexagon[] neighbors = new Hexagon[6];
+
+        int i = 0;
+
+        for(DirectionsHex direction : DirectionsHex.values()) {
+            neighbors[i] = getHexagonNeighbor(x, y, direction);
+            i++;
+        }
+
+        return neighbors;
+    }
+
+    private Hexagon getHexagonNeighbor(int x, int y, DirectionsHex direction){
+        switch(direction){
+            case LEFT:
+                return hexagonArray[x-1][y];
+            case RIGHT:
+                return hexagonArray[x+1][y];
+            case UPPERLEFT:
+                return hexagonArray[x-1 + offset(y)][y+1];
+            case UPPERRIGHT:
+                return hexagonArray[x + offset(y)][y+1];
+            case LOWERLEFT:
+                return hexagonArray[x-1 + offset(y)][y-1];
+            case LOWERRIGHT:
+                return hexagonArray[x + offset(y)][y-1];
+        }
+        // Effectively an Error
+        return hexagonArray[0][0];
+    }
+
+    private int offset(int x){
+        if(x % 2 == 1) {
+            return 1;
+        }else{
+            return 0;
+        }
+    }
+
 }
