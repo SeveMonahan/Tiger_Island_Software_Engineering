a100c61d2d665acf68783e418cbebf77270269d9
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-03-08 02:01:03 -0500

    Added TileBag and TileBagTest functionality.
    Didn't add the "Draw all tiles from a bag" acceptance test as specified in plan.

---
+++ src/TileBag.java
@@ -0,0 +1,44 @@
+import java.util.LinkedList;
+
+public class TileBag {
+
+    private LinkedList<Tile> bag;
+    private int numberOfTilesInBag;
+
+    TileBag() {
+        bag = new LinkedList<Tile>();
+        permutationForAllTiles();
+    }
+
+    Tile drawTile() {
+        numberOfTilesInBag--;
+        return this.bag.pop();
+    }
+
+    LinkedList<Tile> getAllTilesInBag() {
+        return this.bag;
+    }
+
+    int getNumberOfTilesInBag() {
+        return numberOfTilesInBag;
+    }
+
+    private void permutationForAllTiles() {
+        for (Terrain terrain_1 : Terrain.values() ) {
+            for ( Terrain terrain_2 : Terrain.values() ) {
+                if( terrain_1 != Terrain.VOLCANO && terrain_1 != Terrain.EMPTY &&
+                        terrain_2 != Terrain.VOLCANO && terrain_2 != Terrain.EMPTY) {
+
+                    Tile newTile = new Tile(terrain_1, terrain_2);
+                    this.bag.push(newTile);
+                    this.bag.push(newTile);
+                    this.bag.push(newTile);
+                }
+
+            }
+        }
+
+        numberOfTilesInBag = bag.size();
+    }
+
+}
---
+++ test/TileBagTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import java.util.NoSuchElementException;
+
+public class TileBagTest {
+    @Test
+    public void drawTileFromBag() {
+        TileBag testBag = new TileBag();
+
+        assert( testBag.drawTile() instanceof Tile );
+        assertEquals(47, testBag.getNumberOfTilesInBag() );
+    }
+
+    @Test
+    public void drawAllTilesFromBag() {
+        TileBag testBag = new TileBag();
+
+        // Checking if all of the tiles are there would be difficult because duplicates
+        // are permitted. Its doable but could be unnecessary...
+        assertEquals(48, testBag.getNumberOfTilesInBag() );
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void drawTileFromEmptyBagFails() throws Exception {
+        TileBag testBag = new TileBag();
+
+        while( testBag.getNumberOfTilesInBag() != 0 ) {
+            testBag.drawTile();
+        }
+
+        testBag.drawTile();
+    }
+}
