43a20f98975affaa505b3cd784f63b1a266c0ba4
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-03-08 15:02:34 -0500

    Added end game functionality and tests. This will most likely have to be refactored later own.

---
+++ src/Game.java
@@ -0,0 +1,20 @@
+public class Game {
+    public Player player1;
+    public Player player2;
+    protected Player winner;
+
+    Game() {
+        player1 = new Player();
+        player2 = new Player();
+    }
+
+    public void endGameConditions() {
+        if( player1.getScore() == -1 || player2.getScore() > player1.getScore() ) {
+            winner = player2;
+        } else if( player2.getScore() == -1 || player1.getScore() > player2.getScore() ) {
+            winner = player1;
+        } else { // Tie game
+            winner = null;
+        }
+    }
+}
---
+++ test/EndGameConditionsTest.java
@@ -0,0 +1,34 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+public class EndGameConditionsTest {
+    @Test
+    public void tieGame() {
+        Game game = new Game();
+        game.endGameConditions();
+
+        assertNull( game.winner );
+    }
+
+    @Test
+    public void pointBasedWin() {
+        Game game = new Game();
+        game.player1.modifyScore(4);
+        game.endGameConditions();
+
+        assert( game.player1.equals( game.winner ));
+    }
+
+    @Test
+    public void autoLoseEndGame() {
+        Game game = new Game();
+
+        game.player1.setAutoLoseScore();
+
+        game.endGameConditions();
+
+        assert( game.player2.equals( game.winner ));
+
+    }
+}
