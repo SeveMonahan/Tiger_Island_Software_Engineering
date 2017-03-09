01a383964e9ef23dd62677dde07d00fc43fcf68f
Cameron M. Durr <cameronmdurr@ufl.edu>
2017-03-09 17:52:30 -0500

    Add the files that I created (Piece, PieceType, Settlement, SettlementTest, TotoroTest)

---
+++ test/SettlementTest.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SettlementTest {
+    @Test
+    public void settlementOfSizeOne() {
+        Board board = new Board(new Tile(Terrain.JUNGLE, Terrain.GRASS));
+        Hexagon hexagon = board.getHexagon(new Coordinate(99,101));
+        Player player = new Player();
+        player.placeMeepleOnHexagon(hexagon);
+        Settlement settlement = new Settlement(hexagon);
+        Assert.assertTrue(settlement.getHexagons().size() == 1);
+    }
+    @Test
+    public void settlementOfSizeTwo() {
+
+    }
+    @Test
+    public void settlementOfSizeOneShouldPreventTilePlacement() {
+
+    }
+}
---
+++ test/TotoroTest.java
@@ -0,0 +1,19 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TotoroTest {
+    @Test
+    public void shouldInitializeToThreeTotoro() {
+        Player player = new Player();
+        Assert.assertTrue(player.getTotoroCount() == 3);
+    }
+    @Test
+    public void totoroShouldStopTilePlacement() {
+        Board board = new Board(new Tile(Terrain.BEACH, Terrain.GRASS));
+        board.placeTile(new Tile(Terrain.ROCK, Terrain.JUNGLE), DirectionsHex.RIGHT, new Coordinate(100,101));
+        Hexagon hexagon = board.getHexagon(new Coordinate(100,100));
+        hexagon.addPiece(new Piece(PieceType.TOTORO));
+        boolean isValidMove = board.placeTile(new Tile(Terrain.ROCK, Terrain.ROCK), DirectionsHex.LOWERRIGHT, new Coordinate(100,101));
+        Assert.assertFalse(isValidMove);
+    }
+}
