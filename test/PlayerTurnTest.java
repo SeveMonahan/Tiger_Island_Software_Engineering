eab07fb92f080ee7826d2d3d266bb04cb7886d44
ajwoo <ajwoo@ufl.edu>
2017-03-10 11:48:02 -0500

    if a player tries to dra twice in a row then his second draw is not valid - first draw is vaild - swtich turns - player one tries to draw twice in a row but is not allowed

--- test/PlayerTurnTest.java
+++ test/PlayerTurnTest.java
@@ -24,4 +24,16 @@ public class PlayerTurnTest {
         assertEquals(true, player1.canDrawFrom(testBag));
     }
 
+    @Test
+    public void canNotDrawFromTwice() throws Exception {
+        TileBag testBag = new TileBag();
+        Player player1 = new Player(Color.BLACK, true);
+        Player player2 = new Player(Color.WHITE, false);
+        //player1 draws and turn is over
+        assertEquals(true, player1.canDrawFrom(testBag));
+        player1.turnSwitch(player2);
+        //player1 tries to draw again but is not allowed
+        assertEquals(false, player1.canDrawFrom(testBag));
+
+    }
 }
