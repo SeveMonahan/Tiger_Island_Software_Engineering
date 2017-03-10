8c0a041d2ee294ba78e1cb66271a660ab60675b7
Cameron M. Durr <cameronmdurr@ufl.edu>
2017-03-09 21:12:25 -0500

    Remove Settlement class

--- test/SettlementTest.java
+++ test/SettlementTest.java
@@ -4,12 +4,7 @@ import org.junit.Test;
 public class SettlementTest {
     @Test
     public void settlementOfSizeOne() {
-        Board board = new Board(new Tile(Terrain.JUNGLE, Terrain.GRASS));
-        Hexagon hexagon = board.getHexagon(new Coordinate(99,101));
-        Player player = new Player(Color.WHITE);
-        player.placeMeepleOnHexagon(hexagon);
-        Settlement settlement = new Settlement(hexagon);
-        Assert.assertTrue(settlement.getHexagons().size() == 1);
+
     }
     @Test
     public void settlementOfSizeTwo() {
