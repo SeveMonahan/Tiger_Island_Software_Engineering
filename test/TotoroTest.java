5a97fe69bc2ed68145fd11bdeb6982640cb739ea
Seve Monahan <monahan.seve@gmail.com>
2017-03-10 03:34:17 -0500

    Fix up totoroShouldStopTilePlacement test (it still fails)
    but now I'm significantly more confident it actually tests what it's
    trying to test (before it tried to put a Totoro on a volcano, for
    example)

--- test/TotoroTest.java
+++ test/TotoroTest.java
@@ -9,15 +9,20 @@ public class TotoroTest {
         Player player = new Player(Color.WHITE);
         assertEquals(3, player.getTotoroCount());
     }
-    /*
+
     @Test
     public void totoroShouldStopTilePlacement() {
         Board board = new Board(new Tile(Terrain.BEACH, Terrain.GRASS));
-        board.placeTile(new Tile(Terrain.ROCK, Terrain.JUNGLE), DirectionsHex.RIGHT, new Coordinate(100,101));
-        Hexagon hexagon = board.getHexagon(new Coordinate(100,100));
-        hexagon.addPiece(new Piece(PieceType.TOTORO, Color.WHITE));
+        assertEquals(true, board.placeTile(new Tile(Terrain.ROCK, Terrain.JUNGLE), DirectionsHex.RIGHT, new Coordinate(100,101)));
+        Hexagon hexagon = board.getHexagon(new Coordinate(101,100));
+
+        Player player = new Player(Color.WHITE);
+        Piece newTotoro = new Totoro(Color.WHITE);
+
+        assertEquals(true, player.attemptToPlacePiece(newTotoro, hexagon));
+
         boolean isValidMove = board.placeTile(new Tile(Terrain.ROCK, Terrain.ROCK), DirectionsHex.LOWERRIGHT, new Coordinate(100,101));
-        Assert.assertFalse(isValidMove);
+
+        assertEquals(false, isValidMove);
     }
-    */
 }
