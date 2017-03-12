3426920ebcaf11ef4a37421c6ddddac24a8368de
mmarc30 <mmarc30@gmail.com>
2017-03-12 18:51:32 -0400

    Added pseudocode to be implemented later, need someone to check if it sounds plausible

--- test/SettlementTest.java
+++ test/SettlementTest.java
@@ -1,14 +1,44 @@
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.Assert.assertEquals;
 
 public class SettlementTest {
+    /*
+    Pseudocode for the Hex.settlementSize():
+    hashmap.add(hex) //I think a hashmap could work as a set of visited hexagons
+    queue.add(hex)
+    while (queue not empty) {
+        current = queue.dequeue();
+        size++;
+        for (every unvisited neighbor of current with population > 0) {
+            enqueue neighbor
+        }
+     }
+     */
     @Test
     public void settlementOfSizeOne() {
-
+        //Not really a real settlement test
+        Player player = new Player(Color.WHITE);
+        Board board = new Board(new Tile(Terrain.BEACH, Terrain.GRASS));
+        Coordinate coordinate = new Coordinate(100,100).getHexagonNeighborCoordinate(HexagonNeighborDirection.LEFT);
+        Hexagon hexagon = board.getHexagon(coordinate);
+        player.placeMeepleOnHexagon(hexagon);
+        assertEquals(1, hexagon.getPopulation());
     }
     @Test
     public void settlementOfSizeTwo() {
+        //Need to implement algorithm above
+        Player player = new Player(Color.WHITE);
+        Board board = new Board(new Tile(Terrain.BEACH, Terrain.GRASS));
+        Coordinate coordinate = new Coordinate(100,100).getHexagonNeighborCoordinate(HexagonNeighborDirection.LEFT);
+        Coordinate coordinate1 = new Coordinate(100,100).getHexagonNeighborCoordinate(HexagonNeighborDirection.UPPERLEFT);
+        Hexagon hexagon = board.getHexagon(coordinate);
+        Hexagon hexagon1 = board.getHexagon(coordinate1);
+        player.placeMeepleOnHexagon(hexagon);
+        player.placeMeepleOnHexagon(hexagon1);
 
     }
     @Test
