85c940b1f1556d1083aa1be6032a21018fcd24d5
mmarc30 <mmarc30@gmail.com>
2017-03-12 19:28:17 -0400

    Half implemented hex.settlementSize()

--- test/SettlementTest.java
+++ test/SettlementTest.java
@@ -1,4 +1,5 @@
 import org.junit.Assert;
+import java.util.*;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -21,11 +22,32 @@ public class SettlementTest {
     @Test
     public void settlementOfSizeOne() {
         //Not really a real settlement test
+        int size = 0;
+        HashMap map = new HashMap();
+        Queue<Hexagon> hexagonQueue = new LinkedList<>();
+        Coordinate currentCoordinate;
+        Hexagon currentHexagon;
         Player player = new Player(Color.WHITE);
         Board board = new Board(new Tile(Terrain.BEACH, Terrain.GRASS));
         Coordinate coordinate = new Coordinate(100,100).getHexagonNeighborCoordinate(HexagonNeighborDirection.LEFT);
         Hexagon hexagon = board.getHexagon(coordinate);
         player.placeMeepleOnHexagon(hexagon);
+        if (hexagon.getPopulation() == 0) {
+            return;
+        }
+        hexagonQueue.add(hexagon);
+        while(!hexagonQueue.isEmpty()) {
+            currentHexagon = hexagonQueue.remove();
+            map.put(currentHexagon.hashCode(),true);
+            size++;
+            //have to create for loop for all neighbors
+            currentCoordinate = coordinate.getHexagonNeighborCoordinate(HexagonNeighborDirection.UPPERLEFT);
+            currentHexagon = board.getHexagon(currentCoordinate);
+            if (currentHexagon.getPopulation() > 0 && map.containsKey(currentHexagon.hashCode())) {
+                map.put(currentHexagon.hashCode(),1);
+            }
+
+        }
         assertEquals(1, hexagon.getPopulation());
     }
     @Test
