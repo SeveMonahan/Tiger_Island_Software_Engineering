cce3a9894fca8ca8b2f78982d1ecfee30a72a15a
ajwoo <ajwoo@ufl.edu>
2017-03-23 11:20:56 -0400

    Tiger placed on volcano test

--- src/TigerIsland/test/TigersTest.java
+++ src/TigerIsland/test/TigersTest.java
@@ -1,9 +1,9 @@
 package TigerIsland.test;
 
-import TigerIsland.Color;
-import TigerIsland.Player;
+import TigerIsland.*;
 import org.junit.Test;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.*;
 
 public class TigersTest {
@@ -11,5 +11,20 @@ public class TigersTest {
     public void shouldInitializeToTwoTigers() throws Exception {
         Player player = new Player(Color.WHITE);
         assertEquals(2, player.getTigerCount());
+    }
+
+
+    @Test
+    public void placeTigerOnVolcano() throws Exception {
+        Board board = new Board(new Tile(Terrain.LAKE, Terrain.GRASSLAND));
+        Coordinate coordinate = new Coordinate(101, 100).getHexagonNeighborCoordinate(HexagonNeighborDirection.LEFT);
+        Hexagon hexagon = board.getHexagon(coordinate);
+        Player player = new Player(Color.WHITE);
+
+        player.placeTigerOnHexagon(hexagon);
+
+        assertEquals(0, player.getScore());
+        assertEquals(2, player.getTigerCount());
+        assertEquals(0, hexagon.getPopulation());
     }
 }
