1a8b812e74d77e62c8772c6a022c499178365af1
Cameron M. Durr <cameronmdurr@ufl.edu>
2017-03-26 14:04:48 -0400

    Delete Color, Coordinate, and Terrain

--- src/TigerIsland/Features/Coordinate/CoordinateDefinitions.java
+++
@@ -1,9 +0,0 @@
-package Coordinate;
-
-import TigerIsland.*;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
-
-public class CoordinateDefinitions {
-}
