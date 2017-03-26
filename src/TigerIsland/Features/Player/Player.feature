40c56c1b3903d5ae83ba63a2ae7a1abca44cabcc
Cameron M. Durr <cameronmdurr@ufl.edu>
2017-03-26 14:03:00 -0400

    Add player scenarios

--- src/TigerIsland/Features/Player/Player.feature
+++ src/TigerIsland/Features/Player/Player.feature
@@ -1,16 +1,13 @@
 Feature: Player
 
 #Player scenario test
-Scenario: Score of initialized player
-    Given I have initialized player
-    When I query that player's score
-    Then I receive 0
+    Scenario: Score of initialized player
+        Given I have initialized a player
+        When I query that player's score
+        Then I receive 0
 
-Scenario: Score of player after addition
-    Given I have initialized a player and added 5 to their score
-    When I query that player's score
-    Then I receive 5
-
-
-
-
+    Scenario: Score of player after addition
+        Given I have initialized a player
+        And I add 5 to that player's score
+        When I query that player's score
+        Then I receive 5
--- src/TigerIsland/Features/Player/PlayerDefinitions.java
+++ src/TigerIsland/Features/Player/PlayerDefinitions.java
@@ -4,6 +4,24 @@ import TigerIsland.*;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.junit.Assert;
 
 public class PlayerDefinitions {
+    private Player player = null;
+    private int score;
+
+    @Given("^I have initialized a player$")
+    public void initPlayer() { player = new Player(Color.BLACK); }
+
+    @Given("^I add 5 to that player's score$")
+    public void addPoints() { player.setScore(player.getScore() + 5); }
+
+    @When("^I query that player's score$")
+    public void queryPlayerScore() { score = player.getScore(); }
+
+    @Then("^I receive 0$")
+    public void verifyPlayerScoreIsZero() { Assert.assertEquals(score, 0); }
+
+    @Then("^I receive 5$")
+    public void verifyPlayerScoreIsFive() { Assert.assertEquals(score, 5); }
 }
