761b72e20409d7c56bd1bb517f9706fcf4bf18a3
Seve Monahan <monahan.seve@gmail.com>
2017-04-02 15:03:15 -0400

    Rename a couple variables

--- src/TigerIsland/Game.java
+++ src/TigerIsland/Game.java
@@ -1,24 +1,24 @@
 package TigerIsland;
 
 public class Game {
-    private Player player1;
-    private Player player2;
+    private Player player_1;
+    private Player player_2;
     private Player playerWhoseTurn;
     private boolean gameIsOver;
 
     public Game() {
-        player1 = new Player(Color.WHITE);
-        player2 = new Player(Color.BLACK);
-        playerWhoseTurn = player1;
+        player_1 = new Player(Color.WHITE);
+        player_2 = new Player(Color.BLACK);
+        playerWhoseTurn = player_1;
         gameIsOver = false;
     }
 
     public Player getPlayerOne(){
-        return player1;
+        return player_1;
     }
 
     public Player getPlayerTwo(){
-        return player2;
+        return player_2;
     }
 
     public boolean isMyTurn(Player me) {
@@ -26,10 +26,10 @@ public class Game {
     }
 
     public void changeTurn() {
-        if (playerWhoseTurn == player1)
-            playerWhoseTurn = player2;
+        if (playerWhoseTurn == player_1)
+            playerWhoseTurn = player_2;
         else
-            playerWhoseTurn = player1;
+            playerWhoseTurn = player_1;
     }
 
     private boolean playerScoreIsLessThanOpponent(Player player1, Player opponent) {
@@ -44,10 +44,10 @@ public class Game {
         Player winner;
 
         if(gameIsOver){
-            if (playerScoreIsLessThanOpponent(player1, player2)) {
-                winner = player2;
-            } else if (playerScoreIsLessThanOpponent(player2, player1)) {
-                winner = player1;
+            if (playerScoreIsLessThanOpponent(player_1, player_2)) {
+                winner = player_2;
+            } else if (playerScoreIsLessThanOpponent(player_2, player_1)) {
+                winner = player_1;
             } else { // Tie game
                 return GameOutcome.TIE;
             }
