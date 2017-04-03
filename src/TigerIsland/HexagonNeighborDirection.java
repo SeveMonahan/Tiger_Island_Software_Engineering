771d82a752d4ac4395ed176342d4b85ccdae2ca9
marc <mmarc30@gmail.com>
2017-04-03 16:22:17 -0400

    network stuff

--- src/TigerIsland/HexagonNeighborDirection.java
+++ src/TigerIsland/HexagonNeighborDirection.java
@@ -27,4 +27,41 @@ public enum HexagonNeighborDirection {
         // Error result
         return HexagonNeighborDirection.LEFT;
     }
+
+    public HexagonNeighborDirection intToDirection(int x) {
+        switch (x) {
+            case 1:
+                return UPPERLEFT;
+            case 2:
+                return UPPERRIGHT;
+            case 3:
+                return RIGHT;
+            case 4:
+                return LOWERRIGHT;
+            case 5:
+                return LOWERLEFT;
+            case 6:
+                return LEFT;
+        }
+
+        return HexagonNeighborDirection.LEFT;
+    }
+
+    public int directionToInt(HexagonNeighborDirection dir) {
+        switch (dir) {
+            case UPPERLEFT:
+                return 1;
+            case UPPERRIGHT:
+                return 2;
+            case RIGHT:
+                return 3;
+            case LOWERRIGHT:
+                return 4;
+            case LOWERLEFT:
+                return 5;
+            case LEFT:
+                return 6;
+        }
+        return 1;
+    }
 }
