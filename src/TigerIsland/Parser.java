94f9bdf61ba9e70eb5eecd8948f46c6b814110d2
Evan Renz <erenz@ufl.edu>
2017-04-04 16:29:33 -0400

    Created a parser function and test for Taking in "GAME <gid> MOVE <#> PLAYER <pid> <place> <build>" and outputting a TileMove object

--- src/TigerIsland/Parser.java
+++ src/TigerIsland/Parser.java
@@ -2,15 +2,43 @@ package TigerIsland;
 
 public class Parser {
 
-    //Will eventually return a tileMove
+    //Takes in "MAKE YOUR MOVE IN GAME <gid> WITHIN <timemove> SECOND: MOVE <#> PLACE <tile>" and outputs Tile object
     public Tile makeYourMoveStringToTile(String makeYourMoveString) {
         String[] moveStringSplitBySpaceArray = makeYourMoveString.split("\\s+");
-        String tileString = moveStringSplitBySpaceArray[12];
 
+        String tileString = moveStringSplitBySpaceArray[12];
+        return tileStringToTile(tileString);
+    }
+
+
+    //Takes in "GAME <gid> MOVE <#> PLAYER <pid> <place> <build>" and outputs a TileMove object
+    public TileMove opponentMoveStringToTileMove(String opponentMoveString){
+        String[] opponentMoveStringSplitBySpaceArray = opponentMoveString.split("\\s+");
+
+        String tileString = opponentMoveStringSplitBySpaceArray[7];
+        Tile tile = tileStringToTile(tileString);
+
+        int x = Integer.parseInt(opponentMoveStringSplitBySpaceArray[9]);
+        int y = Integer.parseInt(opponentMoveStringSplitBySpaceArray[10]);
+        int z = Integer.parseInt(opponentMoveStringSplitBySpaceArray[11]);
+        Coordinate coordinate = new Coordinate(x, y, z);
+
+        int orientation = Integer.parseInt(opponentMoveStringSplitBySpaceArray[12]);
+
+        //Assign UPPERLEFT direction as arbitrary only to be converted to its actual direction
+        //This has to be done because you can not create a null instance of an enum
+        HexagonNeighborDirection direction = HexagonNeighborDirection.UPPERLEFT;
+        direction = direction.intToDirection(orientation);
+
+        return new TileMove(tile, direction, coordinate);
+    }
+
+
+    //Converts string such as "JUNGLE+LAKE" to a tile object
+    private Tile tileStringToTile(String tileString){
         String [] tileStringSplitByPlusSign = tileString.split("\\+");
         Terrain Aterrain = Terrain.valueOf(tileStringSplitByPlusSign[0]);
         Terrain Bterrain = Terrain.valueOf(tileStringSplitByPlusSign[1]);
-
         return new Tile(Aterrain, Bterrain);
     }
 }
--- src/TigerIsland/UnitTests/ParserTest.java
+++ src/TigerIsland/UnitTests/ParserTest.java
@@ -6,11 +6,29 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 public class ParserTest {
     @Test
-    public void getTileFromMakeYourMove(){
+    public void getTileFromMakeYourMoveString(){
         String message = "MAKE YOUR MOVE IN GAME <gid> WITHIN <timemove> SECOND: MOVE <#> PLACE JUNGLE+LAKE";
         Tile expectedTestTile = new Tile(Terrain.JUNGLE, Terrain.LAKE);
         Parser parser = new Parser();
         Tile testTile = parser.makeYourMoveStringToTile(message);
         assertEquals(expectedTestTile.getTerrainsClockwiseFromVolcano(), testTile.getTerrainsClockwiseFromVolcano());
+    }
+
+    @Test
+    public void getTileMoveFromGameMoveMade(){
+        String message = "GAME <gid> MOVE <#> PLAYER <pid> PLACED JUNGLE+LAKE AT 0 0 0 1 FOUNDED SETTLEMENT AT <x> <y> <z>";
+        Tile expectedTile = new Tile(Terrain.JUNGLE, Terrain.LAKE);
+        Coordinate expectedCoordinate = new Coordinate(0, 0, 0);
+        TileMove expectedTileMove = new TileMove(expectedTile, HexagonNeighborDirection.UPPERLEFT, expectedCoordinate);
+
+        Parser parser = new Parser();
+        TileMove testTileMove = parser.opponentMoveStringToTileMove(message);
+
+        assertEquals(expectedTileMove.getTile().getTerrainsClockwiseFromVolcano(), testTileMove.getTile().getTerrainsClockwiseFromVolcano());
+        assertEquals(expectedTileMove.getDirection(), testTileMove.getDirection());
+        assertEquals(expectedTileMove.getCoordinate().getX(), testTileMove.getCoordinate().getX());
+        assertEquals(expectedTileMove.getCoordinate().getY(), testTileMove.getCoordinate().getY());
+
+
     }
 }
