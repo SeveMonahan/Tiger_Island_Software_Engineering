03fc7dba2e065841bd1e1b96b5b26eac1dacf732
Evan Renz <erenz@ufl.edu>
2017-04-04 15:15:48 -0400

    Created function and test for parser taking input of "MAKE YOUR MOVE IN GAME" and outputting tile to be moved

--- src/TigerIsland/Parser.java
+++ src/TigerIsland/Parser.java
@@ -2,4 +2,15 @@ package TigerIsland;
 
 public class Parser {
 
+    //Will eventually return a tileMove
+    public Tile makeYourMoveStringToTile(String makeYourMoveString) {
+        String[] moveStringSplitBySpaceArray = makeYourMoveString.split("\\s+");
+        String tileString = moveStringSplitBySpaceArray[12];
+
+        String [] tileStringSplitByPlusSign = tileString.split("\\+");
+        Terrain Aterrain = Terrain.valueOf(tileStringSplitByPlusSign[0]);
+        Terrain Bterrain = Terrain.valueOf(tileStringSplitByPlusSign[1]);
+
+        return new Tile(Aterrain, Bterrain);
+    }
 }
--- src/TigerIsland/UnitTests/ParserTest.java
+++ src/TigerIsland/UnitTests/ParserTest.java
@@ -5,5 +5,12 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 public class ParserTest {
-
+    @Test
+    public void getTileFromMakeYourMove(){
+        String message = "MAKE YOUR MOVE IN GAME <gid> WITHIN <timemove> SECOND: MOVE <#> PLACE JUNGLE+LAKE";
+        Tile expectedTestTile = new Tile(Terrain.JUNGLE, Terrain.LAKE);
+        Parser parser = new Parser();
+        Tile testTile = parser.makeYourMoveStringToTile(message);
+        assertEquals(expectedTestTile.getTerrainsClockwiseFromVolcano(), testTile.getTerrainsClockwiseFromVolcano());
+    }
 }
