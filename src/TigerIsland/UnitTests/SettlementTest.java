efe1a18c4603a710aa897fbe5e65da4066130752
Marc Mendoza <mmarc30@gmail.com>
2017-04-05 11:20:51 -0400

    removed middleman boards from settlement tests

--- src/TigerIsland/UnitTests/SettlementTest.java
+++ src/TigerIsland/UnitTests/SettlementTest.java
@@ -10,10 +10,9 @@ public class SettlementTest {
     public void settlementOfSizeOne() {
         Player player = new Player(Color.WHITE);
         Player playerTwo = new Player(Color.BLACK);
-        Board boardWithTile = new Board();
+        Board board = new Board();
         TileMove startingTileMove = new TileMove(new Tile(Terrain.LAKE, Terrain.GRASSLAND), HexagonNeighborDirection.LEFT, new Coordinate (100, 100));
-        boardWithTile.placeTile(startingTileMove);
-        Board board = boardWithTile;
+        board.placeTile(startingTileMove);
         Coordinate coordinateOne = new Coordinate(100,100).getNeighboringCoordinate(HexagonNeighborDirection.LEFT);
 
         player.placeMeepleOnHexagon(coordinateOne, board);
@@ -28,10 +27,9 @@ public class SettlementTest {
     public void settlementOfSizeTwo() {
         Player player = new Player(Color.WHITE);
         Player playerTwo = new Player(Color.BLACK);
-        Board boardWithTile = new Board();
+        Board board = new Board();
         TileMove startingTileMove = new TileMove(new Tile(Terrain.LAKE, Terrain.GRASSLAND), HexagonNeighborDirection.LEFT, new Coordinate (100, 100));
-        boardWithTile.placeTile(startingTileMove);
-        Board board = boardWithTile;
+        board.placeTile(startingTileMove);
         board.placeTile(new TileMove(new Tile(Terrain.ROCK, Terrain.ROCK), HexagonNeighborDirection.RIGHT, new Coordinate(97, 101)));
         Coordinate playerOneMeepleOne = new Coordinate(99,100);
         Coordinate playerOneMeepleTwo = new Coordinate (99,101);
