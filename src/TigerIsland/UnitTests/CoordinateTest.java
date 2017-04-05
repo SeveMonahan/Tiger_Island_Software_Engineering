1585e1e58bb689946a2e45533e0a9c3fa44608c7
ajwoo <ajwoo@ufl.edu>
2017-04-04 20:59:12 -0400

    Switch the variables

--- src/TigerIsland/UnitTests/CoordinateTest.java
+++ src/TigerIsland/UnitTests/CoordinateTest.java
@@ -1,6 +1,6 @@
 package src.TigerIsland.UnitTests;
 
-import TigerIsland.Coordinate;
+import src.TigerIsland.Coordinate;
 import org.junit.Test;
 
 import static org.junit.Assert.assertArrayEquals;
@@ -53,42 +53,42 @@ public class CoordinateTest {
 
         Coordinate testCoordinate = new Coordinate(101,100);
         int result[] = testCoordinate.ConvertToCube();
-        int testResult[] = {0,1,-1};
+        int testResult[] = {1,-1,0};
         assertArrayEquals(testResult,result);
 
         Coordinate testCoordinate1 = new Coordinate(102,100);
         int result1[] = testCoordinate1.ConvertToCube();
-        int testResult1[] = {0,2,-2};
+        int testResult1[] = {2,-2,0};
         assertArrayEquals(testResult1,result1);
 
         Coordinate testCoordinate2 = new Coordinate(99,100);
         int result2[] = testCoordinate2.ConvertToCube();
-        int testResult2[] = {0,-1,1};
+        int testResult2[] = {-1,1,0};
         assertArrayEquals(testResult2, result2);
 
         Coordinate testCoordinate3 = new Coordinate(100,101);
         int result3[] = testCoordinate3.ConvertToCube();
-        int testResult3[] = {-1,1,0};
+        int testResult3[] = {1,0,-1};
         assertArrayEquals(testResult3, result3);
 
         Coordinate testCoordinate4 = new Coordinate(99,99);
         int result4[] = testCoordinate4.ConvertToCube();
-        int testResult4[] = {1,-1,0};
+        int testResult4[] = {-1,0,1};
         assertArrayEquals(testResult4, result4);
 
         Coordinate testCoordinate6 = new Coordinate(99,101);
         int result6[] = testCoordinate6.ConvertToCube();
-        int testResult6[] = {-1,0,1};
+        int testResult6[] = {0,1,-1};
         assertArrayEquals(testResult6, result6);
 
         Coordinate testCoordinate7 = new Coordinate(101,101);
         int result7[] = testCoordinate7.ConvertToCube();
-        int testResult7[] = {-1,2,-1};
+        int testResult7[] = {2,-1,-1};
         assertArrayEquals(testResult7, result7);
 
         Coordinate testCoordinate9 = new Coordinate(101,99);
         int result9[] = testCoordinate9.ConvertToCube();
-        int testResult9[] = {1,1,-2};
+        int testResult9[] = {1,-2,1};
         assertArrayEquals(testResult9, result9);
     }
 
