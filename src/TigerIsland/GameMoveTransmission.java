a4fe66b3b6e579a22a686b34aa62a59cf21927d8
Evan Renz <erenz@ufl.edu>
2017-04-06 19:05:01 -0400

    Changed gid field to String and added getters for each field

--- src/TigerIsland/GameMoveTransmission.java
+++ src/TigerIsland/GameMoveTransmission.java
@@ -1,12 +1,21 @@
 package TigerIsland;
 
 public class GameMoveTransmission {
-    private int gid;
+    private String gid;
     private int moveNumber;
     private TileMove tileMove;
     private ConstructionMoveTransmission constructionMoveTransmission;
 
-    public GameMoveTransmission(int gid, int moveNumber, TileMove tileMove, ConstructionMoveTransmission constructionMoveTransmission){
+    public String getGid() {return gid;}
+
+    public int getMoveNumber() {return moveNumber;}
+
+    public TileMove getTileMove() {return tileMove;}
+
+    public ConstructionMoveTransmission getConstructionMoveTransmission() { return constructionMoveTransmission;}
+
+
+    public GameMoveTransmission(String gid, int moveNumber, TileMove tileMove, ConstructionMoveTransmission constructionMoveTransmission){
         this.gid = gid;
         this.moveNumber = moveNumber;
         this.tileMove = tileMove;
