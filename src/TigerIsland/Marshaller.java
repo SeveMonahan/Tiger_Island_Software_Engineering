40b88692fd819b8b2f2e8618a15814be333ffa3c
Evan Renz <erenz@ufl.edu>
2017-04-06 14:50:35 -0400

    Created MarshallerTest and Marshaller classes

---
+++ src/TigerIsland/Marshaller.java
@@ -0,0 +1,4 @@
+package TigerIsland;
+
+public class Marshaller {
+}
---
+++ src/TigerIsland/UnitTests/MarshallerTest.java
@@ -0,0 +1,9 @@
+package TigerIsland.UnitTests;
+
+import TigerIsland.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+public class MarshallerTest {
+
+}
