8edfd56dab15c99ee59d901cd1d33d8e7f958a50
Seve Monahan <monahan.seve@gmail.com>
2017-04-06 14:07:54 -0400

    Add boilerplate for ConstructionMoveInternal class

---
+++ src/TigerIsland/ConstructionMoveInternal.java
@@ -0,0 +1,11 @@
+package TigerIsland;
+
+public class ConstructionMoveInternal{
+    private ConstructionMoveTransmission raw;
+    private Player player;
+
+    public ConstructionMoveInternal(ConstructionMoveTransmission raw, Player player) {
+        this.raw = raw;
+        this.player = player;
+    }
+}
