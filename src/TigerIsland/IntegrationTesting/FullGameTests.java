1066007747daddd08d767fb90fe4178e84f1e845
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-07 00:30:18 -0400

    Added integration testing directory and the start of a full game test

---
+++ src/TigerIsland/IntegrationTesting/FullGameTests.java
@@ -0,0 +1,19 @@
+package TigerIsland.IntegrationTesting;
+
+import TigerIsland.*;
+import org.junit.Test;
+
+public class FullGameTests {
+    @Test
+    public void FullGameTest_01() {
+        Board board = new Board();
+        board.placeStartingTile();
+
+        Player player_01 = new Player(Color.BLACK);
+        Player player_02 = new Player(Color.WHITE);
+
+        // Player 1
+
+        // Player 2
+    }
+}
