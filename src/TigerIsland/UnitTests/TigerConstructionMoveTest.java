ba9bd927adcb6f46e69239a0403c82967caf0e89
ajwoo <ajwoo@ufl.edu>
2017-04-08 08:56:57 -0400

    Check to make sure you cant add a Tiger to a settlement with and already exisiting tiger - refactered name

--- src/TigerIsland/UnitTests/TigerConstructionMoveTest.java
+++ src/TigerIsland/UnitTests/TigerConstructionMoveTest.java
@@ -21,13 +21,13 @@ public class TigerConstructionMoveTest {
         assertEquals(upperRight, move2.getCoordinate());
         assertEquals(false, move2.canPerformMove(player_1, board));
 
-        Hexagon levelTwoHexagon = new Hexagon();
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.ROCK);
+        Hexagon levelThreeHexagon = new Hexagon();
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.ROCK);
 
-        assertEquals(3, levelTwoHexagon.getLevel());
-        board.setHexagonAt(upperRight, levelTwoHexagon);
+        assertEquals(3, levelThreeHexagon.getLevel());
+        board.setHexagonAt(upperRight, levelThreeHexagon);
 
         assertEquals(2, player_1.getTigerCount());
         assertEquals(true, move2.canPerformMove(player_1, board));
@@ -46,12 +46,12 @@ public class TigerConstructionMoveTest {
 
         TigerConstructionMove move2 = new TigerConstructionMove(upperRight);
         assertEquals(false, move2.canPerformMove(player_1, board));
-        Hexagon levelTwoHexagon = new Hexagon();
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.ROCK);
-        assertEquals(3, levelTwoHexagon.getLevel());
-        board.setHexagonAt(upperRight, levelTwoHexagon);
+        Hexagon levelThreeHexagon = new Hexagon();
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.ROCK);
+        assertEquals(3, levelThreeHexagon.getLevel());
+        board.setHexagonAt(upperRight, levelThreeHexagon);
         assertEquals(2, player_1.getTigerCount());
         assertEquals(true, move2.canPerformMove(player_1, board));
 
@@ -75,13 +75,53 @@ public class TigerConstructionMoveTest {
 
         TigerConstructionMove move2 = new TigerConstructionMove(upperRight);
         assertEquals(false, move2.canPerformMove(player_1, board));
-        Hexagon levelTwoHexagon = new Hexagon();
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
-        levelTwoHexagon.changeTerrainTypeThoughExplosion(Terrain.VOLCANO);
-        assertEquals(3, levelTwoHexagon.getLevel());
-        board.setHexagonAt(upperRight, levelTwoHexagon);
+        Hexagon levelThreeHexagon = new Hexagon();
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.VOLCANO);
+        assertEquals(3, levelThreeHexagon.getLevel());
+        board.setHexagonAt(upperRight, levelThreeHexagon);
         assertEquals(2, player_1.getTigerCount());
         assertEquals(false, move2.canPerformMove(player_1, board));
+    }
+    @Test
+    public void cantPlaceTigerWithExsistingTigerInSettlement() {
+        Board board = new Board();
+        board.placeStartingTile();
+        Coordinate upperRight = new Coordinate(100, 100).getNeighboringCoordinateAt(HexagonNeighborDirection.UPPERRIGHT);
+        Color Player_Color = Color.WHITE;
+        Player player_1 = new Player(Player_Color);
+        Coordinate right = new Coordinate(100, 100).getNeighboringCoordinateAt(HexagonNeighborDirection.RIGHT);
+        FoundSettlementConstructionMove move1 = new FoundSettlementConstructionMove(right);
+        move1.makePreverifiedMove(player_1, board);
+        assertEquals(1,player_1.getScore());
+
+        TigerConstructionMove move2 = new TigerConstructionMove(upperRight);
+        assertEquals(false, move2.canPerformMove(player_1, board));
+        Hexagon levelThreeHexagon = new Hexagon();
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.GRASS);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
+        levelThreeHexagon.changeTerrainTypeThoughExplosion(Terrain.ROCK);
+        assertEquals(3, levelThreeHexagon.getLevel());
+        board.setHexagonAt(upperRight, levelThreeHexagon);
+        assertEquals(2, player_1.getTigerCount());
+        assertEquals(true, move2.canPerformMove(player_1, board));
+
+        TigerConstructionMove move3 = new TigerConstructionMove(upperRight);
+        move3.makePreverifiedMove(player_1, board);
+        assertEquals(1, player_1.getTigerCount());
+        assertEquals(76, player_1.getScore());
+
+        Coordinate testCordinate = upperRight.getNeighboringCoordinateAt(HexagonNeighborDirection.RIGHT);
+        Hexagon levelThreeHexagon2 = new Hexagon();
+        levelThreeHexagon2.changeTerrainTypeThoughExplosion(Terrain.GRASS);
+        levelThreeHexagon2.changeTerrainTypeThoughExplosion(Terrain.JUNGLE);
+        levelThreeHexagon2.changeTerrainTypeThoughExplosion(Terrain.ROCK);
+        TigerConstructionMove move4 = new TigerConstructionMove(testCordinate);
+        assertEquals(3, levelThreeHexagon2.getLevel());
+        board.setHexagonAt(upperRight, levelThreeHexagon2);
+        assertEquals(1, player_1.getTigerCount());
+        assertEquals(false, move4.canPerformMove(player_1, board));
+
     }
 }
