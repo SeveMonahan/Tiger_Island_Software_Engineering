d10d1a16ced1ba0b10a72a1ab1aad0466597b91e
ajwoo <ajwoo@ufl.edu>
2017-04-09 00:45:34 -0400

    Develop settlement fails because no valid adjacent hexagon selected

--- src/TigerIsland/UnitTests/ExpandSettlementConstructionMoveTest.java
+++ src/TigerIsland/UnitTests/ExpandSettlementConstructionMoveTest.java
@@ -60,4 +60,24 @@ public class ExpandSettlementConstructionMoveTest {
         assertEquals(false, isSuccess);
         assertEquals(1, player.getMeeplesCount());
     }
+    @Test
+    public void expandSettlementFailsDueToMismatchTerrain() throws Exception {
+        Board board = startBoard();
+
+        Coordinate center = new Coordinate(100, 100);
+        Coordinate LowerRight = center.getNeighboringCoordinateAt(HexagonNeighborDirection.LOWERRIGHT);
+
+        Color Player_Color = Color.WHITE;
+        Player player_1 = new Player(Player_Color);
+
+        // Place a meeple down on LowerRight.
+        FoundSettlementConstructionMove move1 = new FoundSettlementConstructionMove(LowerRight);
+        assertEquals(true, move1.canPerformMove(player_1, board));
+        move1.makePreverifiedMove(player_1, board);
+
+        /// Expand the settlement to the adjacent Jungle hexagon.
+        ExpandSettlementConstructionMove move2 = new ExpandSettlementConstructionMove(LowerRight, Terrain.JUNGLE);
+        assertEquals(false, move2.canPerformMove(player_1, board));
+        move2.makePreverifiedMove(player_1, board);
+    }
 }
