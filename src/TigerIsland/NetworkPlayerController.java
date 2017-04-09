b7aa341c8858888c2ba05f783f5dec4370155be8
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-09 13:43:09 -0400

    Added ability to retreive move from PostMan to NetworkPlayerController

--- src/TigerIsland/NetworkPlayerController.java
+++ src/TigerIsland/NetworkPlayerController.java
@@ -8,6 +8,7 @@ public class NetworkPlayerController implements PlayerController {
         this.color = color;
     }
 
+    @Deprecated
     public GameStateEndOfTurn newGameState(GameStateWTile gameStateWTile){
         Tile tile = null;
         HexagonNeighborDirection hexagonNeighborDirection = null;
@@ -23,24 +24,41 @@ public class NetworkPlayerController implements PlayerController {
 
     // PostMan constructor
     public GameStateEndOfTurn newGameState(GameStateWTile gameStateWTile, PostMan postMan, String gameID){
-        GameMoveIncomingTransmission gameMoveIncomingTransmission = postMan.accessNetworkMailBox();
+        GameMoveIncomingTransmission gameMoveIncomingTransmission = postMan.accessNetworkMailBox(gameID);
         while(gameMoveIncomingTransmission == null) {
             try {
                 postMan.wait();
-                postMan.accessNetworkMailBox(gameID);
+                gameMoveIncomingTransmission = postMan.accessNetworkMailBox(gameID);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
 
-        Tile tile = null;
-        HexagonNeighborDirection hexagonNeighborDirection = null;
-        Coordinate coordinate = null;
+        Tile tile = gameMoveIncomingTransmission.getTileMove().getTile();
+        HexagonNeighborDirection hexagonNeighborDirection = gameMoveIncomingTransmission.getTileMove().getDirection();
+        Coordinate coordinate = gameMoveIncomingTransmission.getTileMove().getCoordinate();
         TileMove tileMove = new TileMove(tile, hexagonNeighborDirection, coordinate);
         GameStateBeforeBuildAction gameStateBeforeBuildAction = GameStateBeforeBuildAction.createGameStateBeforeBuildAction(gameStateWTile, tileMove);
+        gameMoveIncomingTransmission.getConstructionMoveTransmission();
+        Coordinate coordinate1 = gameMoveIncomingTransmission.getConstructionMoveTransmission().getCoordinate();
+        ConstructionMoveInternal constructionMove = null;
 
-        ConstructionMoveInternal constructionMove = null; // Most likely will come from Parser somehow
+        switch (gameMoveIncomingTransmission.getConstructionMoveTransmission().getBuildOption()){
+            case BUILDTIGER:
+                constructionMove = new TigerConstructionMove(coordinate1);
+            case EXPANDSETTLEMENT:
+                ExpandSettlementMoveTransmission expandSettlementMoveTransmission = (ExpandSettlementMoveTransmission) gameMoveIncomingTransmission.getConstructionMoveTransmission();
+                Terrain terrain = expandSettlementMoveTransmission.getTerrain();
+                constructionMove = new ExpandSettlementConstructionMove(coordinate1, terrain);
+            case FOUNDSETTLEMENT:
+                constructionMove = new FoundSettlementConstructionMove(coordinate1);
+            case BUILDTOTORO:
+                constructionMove = new TotoroConstructionMove(coordinate1);
+            case UNABLETOBUILD:
+                constructionMove = null;
+        }
         GameStateEndOfTurn gameStateEndOfTurn = GameStateEndOfTurn.createGameStateFromConstructionMove(gameStateBeforeBuildAction, constructionMove);
+
 
         return gameStateEndOfTurn;
     }
