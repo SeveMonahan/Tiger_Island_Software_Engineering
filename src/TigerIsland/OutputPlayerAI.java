6b5d035b53571e1fbb5d316aa49ff16055592fc4
Seve Monahan <monahan.seve@gmail.com>
2017-04-11 04:17:41 -0400

    Fix a bug in NetworkServerTEst

--- src/TigerIsland/OutputPlayerAI.java
+++ src/TigerIsland/OutputPlayerAI.java
@@ -12,14 +12,12 @@ public class OutputPlayerAI implements OutputPlayerActions{
     }
 
     public void dispatchInformation(GameStateEndOfTurn gameStateEndOfTurn){
-        if(gameStateEndOfTurn.getActivePlayer().getColor() == color) {
             String moveNumber = "**********move_id**********";
             TileMove tileMove = gameStateEndOfTurn.getLastTileMove();
             ConstructionMoveInternal constructionMoveInternal = gameStateEndOfTurn.getLastConstructionMove();
             GameMoveOutgoingTransmission gameMoveOutgoingTransmission = new GameMoveOutgoingTransmission(gid, moveNumber, tileMove, constructionMoveInternal);
 
             postMan.mailAIMessages(gameMoveOutgoingTransmission);
-        }
     }
 
 }
