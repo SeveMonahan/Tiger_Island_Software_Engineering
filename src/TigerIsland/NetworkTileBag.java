d155975bc97674e398ac5be705481934a32f1dca
Jason Cochran <j.a.cochran.cs@gmail.com>
2017-04-14 14:05:39 -0400

    Get rid of bullshit

--- src/TigerIsland/NetworkTileBag.java
+++ src/TigerIsland/NetworkTileBag.java
@@ -1,31 +1,20 @@
 package TigerIsland;
 
-import java.util.LinkedList;
-
 public class NetworkTileBag implements TileBag {
-    private LinkedList<Tile> bag;
     private int numberOfTilesInBag;
     private PostMan tileBagPostMan;
     private String gameID;
 
-    // Constructors
     public NetworkTileBag(PostMan postMan, String gameID) {
-        bag = new LinkedList<Tile>();
         this.tileBagPostMan = postMan;
         this.gameID = gameID;
         numberOfTilesInBag = 48;
     }
 
-    // Methods
-    // Do we still need these "Override" keywords? Or can we delete them? -Cameron
     @Override
     public Tile drawTile() {
         numberOfTilesInBag--;
         return tileBagPostMan.accessTileMailBox(gameID);
-    }
-
-    public LinkedList<Tile> getAllTilesInBag() {
-        return this.bag;
     }
 
     @Override
